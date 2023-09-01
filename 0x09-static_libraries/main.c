0acc0502fc53dac7ca91309f793aca51095bee45
Jael Jepchumba <jaeljepchumba@gmail.com>
2023-09-01 03:55:35 -0700

    A library is not a luxury but one of the necessities of life

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - Entry point of the program.
+ *
+ * This function prints a quote by Kevin Mitnick.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+_puts("\"My goal was to be the best hacker\"\n\t- Kevin Mitnick");
+return (0);
+}
